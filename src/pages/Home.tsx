cadbc530b2b4177d5c021a6dd64ad7c556f434d3
juanimolfino <juanymolfino@hotmail.com>
2026-04-20 23:07:00 -0500

    correccion

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,11 @@
-import { NextStops } from '../components/sections/nextStops';
 import {
   Hero,
   AboutSection,
   RecentPosts,
   DestinationsSection,
 } from '../components/sections'
+
+import { NextStops } from '@/components/sections/nextStops';
 
 export function Home() {
   return (
